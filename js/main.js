9ea165cd2615e598bc125058e0832ea41e04fd57
kelvin <kelvinlightmare@gmail.com>
2015-02-27 14:47:19 +0100

    Woutel branding, text and structure

--- js/main.js
+++ js/main.js
@@ -2,9 +2,39 @@ $(document).ready(function() {
 
 	// Preloader
 	$(window).load(function(){
-		$('.preloader').fadeOut();
+		$('.preloader').fadeOut();		
 	});
 
+	// Datepicker Options  
+	var nowTemp = new Date();
+	var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+	 
+	var checkin = $('#datein').datepicker({		
+	  onRender: function(date) {
+		return date.valueOf() < now.valueOf() ? 'disabled' : '';
+	  }
+	}).on('changeDate', function(ev) {
+	  if (ev.date.valueOf() > checkout.date.valueOf()) {
+		var newDate = new Date(ev.date)
+		newDate.setDate(newDate.getDate() + 1);
+		checkout.setValue(newDate);
+	  }
+	  checkin.hide();
+	  $('#dateout')[0].focus();
+	}).data('datepicker');
+	var checkout = $('#dateout').datepicker({
+	  onRender: function(date) {
+		return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+	  }
+	}).on('changeDate', function(ev) {
+	  checkout.hide();
+	}).data('datepicker');
+
+
+
+	///
+
+
 	// Initiat WOW.js
 	var wow = new WOW(
 	  {
